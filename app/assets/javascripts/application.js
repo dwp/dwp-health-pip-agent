bd81bee90ff3b02b5302fdf7e116e4f0af55b9a3
MMWDP <mark.mccormick@engineering.digital.dwp.gov.uk>
2024-01-23 10:34:36 +0000

    MM: Copy button

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,31 @@ $(document).ready(function () {
   window.GOVUKFrontend.initAll()
 })
 
-import CopyCodeButton from './copy-code-button.js';
+function CopyCodeButton($module) {
+  this.$module = $module;
+}
+
+CopyCodeButton.prototype.init = function () {
+  if (!this.$module) {
+    return;
+  }
+
+  this.$module.addEventListener('click', () => {
+    let code;
+    this.$module.textContent = 'Code copied';
+    setTimeout(() => {
+      this.$module.textContent = 'Copy code';
+    }, 2000);
+    const openPanels = Array.from(document.getElementsByClassName('app-js-visible'));
+    openPanels.forEach((el) => {
+      const panelCopyBtn = this.$module.dataset.module;
+      if (el.id === panelCopyBtn) {
+        code = el.querySelector('pre').textContent;
+        navigator.clipboard.writeText(code);
+      }
+    });
+  });
+};
 
 document.querySelectorAll('.app-example__copy-code-button').forEach((button) => {
   new CopyCodeButton(button).init();
