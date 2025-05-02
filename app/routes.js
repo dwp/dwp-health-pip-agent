0a6cbe5e5a64f2ec0910125da0f0aa0e3e85f494
Mark McCormick <mark.mccormick@engineering.digital.dwp.gov.uk>
2025-05-02 09:15:35 +0100

    MM: V4 of stats

--- app/routes.js
+++ app/routes.js
@@ -51,6 +51,7 @@ router.use('/move-to-pipcs-1', require('./routes/move-to-pipcs-1/router.js'));
 router.use('/agent-stats-0-1', require('./routes/agent-stats-0-1/router.js'));
 router.use('/agent-stats-0-2', require('./routes/agent-stats-0-2/router.js'));
 router.use('/agent-stats-0-3', require('./routes/agent-stats-0-3/router.js'));
+router.use('/agent-stats-0-4', require('./routes/agent-stats-0-4/router.js'));
 
 router.use('/another-decision-0-0-1', require('./routes/another-decision-0-0-1/router.js'));
 router.use('/another-decision-0-0-2', require('./routes/another-decision-0-0-2/router.js'));
