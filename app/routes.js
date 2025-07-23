d9fb2cceec5bcd0364be80f9c68dc50bf6aa6f93
Adam Moss <2951486+adam-moss@users.noreply.github.com>
2025-07-23 17:18:16 +0100

    chore: apply patch
    Signed-off-by: Adam Moss <2951486+adam-moss@users.noreply.github.com>

--- app/routes.js
+++ app/routes.js
@@ -44,7 +44,8 @@ router.use('/agent-day-0-1-5a', require('./routes/agent-day-0-1-5a/router.js'));
 router.use('/agent-day-0-1-6', require('./routes/agent-day-0-1-6/router.js'));
 router.use('/agent-day-0-1-7', require('./routes/agent-day-0-1-7/router.js'));
 router.use('/agent-day-0-1-8', require('./routes/agent-day-0-1-8/router.js'));
-router.use('/agent-day-0-1-9', require('./routes/agent-day-0-1-9/router.js'));
+
+router.use('/agent-day-0-2-1', require('./routes/agent-day-0-2-1/router.js'));
 
 
 router.use('/move-to-pipcs-1', require('./routes/move-to-pipcs-1/router.js'));
