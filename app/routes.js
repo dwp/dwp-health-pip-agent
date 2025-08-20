fc1aa7d023b7795b0a95ca25dc1f034fb1b0ba73
Mark McCormick <mark.mccormick@engineering.digital.dwp.gov.uk>
2025-08-20 17:58:29 +0100

    chore: process mr v4 and ur fixes
    GitOrigin-RevId: 7bc0691e0e4ae0da9f2210a0fa9fddfe1839ca21

--- app/routes.js
+++ app/routes.js
@@ -58,6 +58,7 @@ router.use('/agent-stats-0-4', require('./routes/agent-stats-0-4/router.js'));
 router.use('/another-decision-0-0-1', require('./routes/another-decision-0-0-1/router.js'));
 router.use('/another-decision-0-0-2', require('./routes/another-decision-0-0-2/router.js'));
 router.use('/another-decision-0-0-3', require('./routes/another-decision-0-0-3/router.js'));
+router.use('/another-decision-0-0-4', require('./routes/another-decision-0-0-4/router.js'));
 
 router.use('/person-record-0-0-1', require('./routes/person-record-0-0-1/router.js'));
 router.use('/person-record-0-0-2', require('./routes/person-record-0-0-2/router.js'));
