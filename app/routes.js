1d22de07ac51de2c3521147a0b7e8f2f5cb0c761
Mark McCormick <mark.mccormick@engineering.digital.dwp.gov.uk>
2025-11-10 15:54:19 +0000

    chore: person record update
    GitOrigin-RevId: 1339cf2164fef015a872fa80b5b47810d92d3fe8

--- app/routes.js
+++ app/routes.js
@@ -65,6 +65,7 @@ router.use('/another-decision-0-0-6', require('./routes/another-decision-0-0-6/r
 
 router.use('/person-record-0-0-1', require('./routes/person-record-0-0-1/router.js'));
 router.use('/person-record-0-0-2', require('./routes/person-record-0-0-2/router.js'));
+router.use('/person-record-0-0-3', require('./routes/person-record-0-0-3/router.js'));
 
 router.use('/documents-0-1', require('./routes/documents-0-1/router.js'));
 
